85b7cc636254dabacf490db74f32b45c298da3ac
Messan93 <messan@hotmail.com>
2020-06-17 21:00:21 -0400

    setup of aylien

--- src/client/index.js
+++ src/client/index.js
@@ -4,6 +4,7 @@ import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
+import './styles/resets.scss';
 
 console.log(checkForName);
 
--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,32 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 var path = require('path')
 const express = require('express')
+var AYLIENTextAPI = require('aylien_textapi');
 const mockAPIResponse = require('./mockAPI.js')
 
+// set aylien API credentials
+var textapi = new AYLIENTextAPI({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+  });
+
 const app = express()
+
+/* Dependecies */
+const bodyParser = require('body-parser')
+
+/* Middleware*/
+// Here we are configuring express to use body-parser as middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Cors for cross origin allowance
+const cors = require('cors')
+app.use(cors())
+
+// Intialize the main project folder
 
 app.use(express.static('dist'))
 
